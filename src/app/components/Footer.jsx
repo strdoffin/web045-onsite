d7d58bdaab171fb072793fccc2b08b386d1de1aa
f3xlqz <f3xlqz1212312121@gmail.com>
2025-05-11 14:07:04 +0700

    hahahv2

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
           <div>
             <div className="flex items-center mb-3">
               <img
-                src="https://sdmntprcentralus.oaiusercontent.com/files/00000000-6dc8-61f5-9260-503b298ade46/raw?se=2025-05-11T06%3A34%3A13Z&sp=r&sv=2024-08-04&sr=b&scid=00000000-0000-0000-0000-000000000000&skoid=04233560-0ad7-493e-8bf0-1347c317d021&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-10T20%3A30%3A44Z&ske=2025-05-11T20%3A30%3A44Z&sks=b&skv=2024-08-04&sig=VzgRWqpCHEKHQBzK1XlWftSxSpZGnbp6ARqtsuUe8Cg%3D"
+                src="favicon.ico"
                 alt="425 Logo"
                 className="h-10 mr-2"
               />
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -10,7 +10,7 @@ const Navbar = () => {
           {/* Logo */}
           <a href="/" className="flex items-center space-x-2">
             <span className="w-[64px] h-[64px]">
-              <img src="https://sdmntprcentralus.oaiusercontent.com/files/00000000-6dc8-61f5-9260-503b298ade46/raw?se=2025-05-11T06%3A34%3A13Z&sp=r&sv=2024-08-04&sr=b&scid=00000000-0000-0000-0000-000000000000&skoid=04233560-0ad7-493e-8bf0-1347c317d021&sktid=a48cca56-e6da-484e-a814-9c849652bcb3&skt=2025-05-10T20%3A30%3A44Z&ske=2025-05-11T20%3A30%3A44Z&sks=b&skv=2024-08-04&sig=VzgRWqpCHEKHQBzK1XlWftSxSpZGnbp6ARqtsuUe8Cg%3D" alt="" />
+              <img src="favicon.ico" alt="" />
             </span>
           </a>
 
